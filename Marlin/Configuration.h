c07f6a1d4e6aa5a13f1e6d7228ba44843592b064
RecuitKen <84618709+RecuitKen@users.noreply.github.com>
2021-06-27 23:54:08 +0800

    Changed feedrate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -143,6 +143,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
+#define CUSTOM_MACHINE_NAME "Hexapod ramps testing feedrates"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
@@ -360,6 +361,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
+#define DEFAULT_MAX_FEEDRATE          { 1600, 1600, 1600, 1600, 1600, 1600 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
